e2a8ffa5dce451a344753463fafea0e2b9406cea
Michael Bestas <mikeioannina@gmail.com>
2014-02-16 15:35:59 +0200

    FM: Cleanup resources
    * Remove unused layouts/drawables
    * Remove unused strings
    * Remove CAF chinese translations
    * Improve strings
    * Move debug strings/arrays to debug_strings.xml/debug_arrays.xml
    * Move transmitter strings to transmit_strings.xml
    
    Change-Id: I5b391b459aefa75a6c3edbc525d471a3bcefb2be
    
    FM: fixup strings
    
    * Remove extra space
    * Rename "Czech"  to "Czech Republic", as it really named
    
    Change-Id: Iba9dfedd9a4815a2c06ef3bc8b09ce6fd8a38b56
    
    fmapp2: String improvements
    
    Change-Id: I3ac560a32437eac593fa8274b9b0a2c1c96a468e

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -677,14 +677,12 @@ public class FMRadio extends Activity
           item = menu.add(0, MENU_RECORD_START, 0, R.string.menu_record_start)
                               .setIcon(R.drawable.ic_menu_record);
           if (item != null) {
-             item.setVisible(true);
-             item.setEnabled((!recording) && radioOn);
+             item.setVisible((!recording) && radioOn);
           }
           item = menu.add(0, MENU_RECORD_STOP, 0, R.string.menu_record_stop)
                               .setIcon(R.drawable.ic_menu_record);
           if (item != null) {
-             item.setVisible(true);
-             item.setEnabled(recording && radioOn);
+             item.setVisible(recording && radioOn);
           }
       }
       /* Settings can be active */
@@ -1258,7 +1256,7 @@ public class FMRadio extends Activity
                                                 R.layout.alert_dialog_text_entry, null);
       dlgBuilder.setTitle(R.string.dialog_presetlist_rename_title);
       dlgBuilder.setView(textEntryView);
-      dlgBuilder.setPositiveButton(R.string.alert_dialog_ok,
+      dlgBuilder.setPositiveButton(android.R.string.ok,
                                    new DialogInterface.OnClickListener() {
                                       public void onClick(DialogInterface dialog, int whichButton) {
                                          //int curList = FmSharedPreferences.getCurrentListIndex();
@@ -1277,7 +1275,7 @@ public class FMRadio extends Activity
                                          closeDialog(DIALOG_PRESET_RENAME);
                                       }
                                    });
-      dlgBuilder.setNegativeButton(R.string.alert_dialog_cancel,
+      dlgBuilder.setNegativeButton(android.R.string.cancel,
                                    new DialogInterface.OnClickListener() {
                                       public void onClick(DialogInterface dialog, int whichButton) {
                                          closeDialog(DIALOG_PRESET_RENAME);
@@ -1291,7 +1289,7 @@ public class FMRadio extends Activity
          dlgBuilder.setIcon(R.drawable.alert_dialog_icon)
                    .setTitle(R.string.fm_command_timeout_title);
          dlgBuilder.setMessage(R.string.fm_tune_timeout_msg);
-         dlgBuilder.setPositiveButton(R.string.alert_dialog_ok,
+         dlgBuilder.setPositiveButton(android.R.string.ok,
                                       new DialogInterface.OnClickListener() {
                                          public void onClick(DialogInterface dialog,
                                                              int whichButton) {
@@ -1310,7 +1308,7 @@ public class FMRadio extends Activity
                 .setTitle(R.string.fm_command_failed_title);
       dlgBuilder.setMessage(R.string.fm_cmd_failed_msg);
 
-      dlgBuilder.setPositiveButton(R.string.alert_dialog_ok,
+      dlgBuilder.setPositiveButton(android.R.string.ok,
                                    new DialogInterface.OnClickListener() {
                                       public void onClick(DialogInterface dialog,
                                                           int whichButton) {
@@ -1328,7 +1326,7 @@ public class FMRadio extends Activity
                  .setTitle(R.string.fm_command_failed_title);
        dlgBuilder.setMessage(R.string.fm_cmd_failed_call_on);
 
-       dlgBuilder.setPositiveButton(R.string.alert_dialog_ok,
+       dlgBuilder.setPositiveButton(android.R.string.ok,
                                     new DialogInterface.OnClickListener() {
                                        public void onClick(DialogInterface dialog,
                                                            int whichButton) {
