443f68bbc465f71a96f4943fa26ba4377c406906
linuxx <joey@cyanogenmoditalia.it>
2015-02-14 23:52:14 +0100

    FM: materialize
    * Update colors
    * Vectors
    * Off state messages
    
    Change-Id: I0186edda6c408fcde3dd98cd886ced775adbd938
    Signed-off-by: linuxx <joey@cyanogenmoditalia.it>
    
    FM: Use some sane colors
    
    Pink color on dark background? No thanks!
    
    Change-Id: Ie5a0129c1210aa52deb302c272a7a515a7b27ff9

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -224,6 +224,8 @@ public class FMRadio extends Activity
    private TextView mSleepMsgTV;
    private TextView mRecordingMsgTV;
 
+   private ImageView mFmSeeker;
+
    private double mOutputFreq;
    private int mPresetPageNumber = 0;
    private int mStereo = -1;
@@ -361,6 +363,8 @@ public class FMRadio extends Activity
       if (mBackButton != null) {
           mBackButton.setOnClickListener(mBackClickListener);
       }
+
+      mFmSeeker = (ImageView)findViewById(R.id.fm_seeker);
 
       /* 6 Preset Buttons */
       mPresetButtons[0] = (Button)findViewById(R.id.presets_button_1);
@@ -1830,6 +1834,7 @@ public class FMRadio extends Activity
           setMuteModeButtonImage(false);
       }
       if (bEnable) {
+         mFmSeeker.setVisibility(View.VISIBLE);
          if (mRadioTextScroller != null) {
              mRadioTextScroller.startScroll();
          }
@@ -1854,6 +1859,7 @@ public class FMRadio extends Activity
              }
         }
       }else {
+         mFmSeeker.setVisibility(View.INVISIBLE);
          if (mRadioTextScroller != null) {
              mRadioTextScroller.stopScroll();
          }
@@ -1862,7 +1868,7 @@ public class FMRadio extends Activity
          }
          for (int nButton = 0; nButton < MAX_PRESETS_PER_PAGE; nButton++) {
              if (mPresetButtons[nButton] != null) {
-                  mPresetButtons[nButton].setTextColor(Color.BLACK);
+                  mPresetButtons[nButton].setTextColor(Color.WHITE);
              }
          }
       }
@@ -1898,10 +1904,6 @@ public class FMRadio extends Activity
          mRecordingMsgTV.setVisibility(((bEnable == true) ? View.VISIBLE
                                      : View.GONE));
       }
-      if (mRadioTextTV != null) {
-         mRadioTextTV.setVisibility(((bEnable == true) ? View.VISIBLE
-                                  : View.GONE));
-      }
       if(mERadioTextTV != null) {
          mERadioTextTV.setVisibility(((bEnable == true) ? View.VISIBLE
                                   : View.GONE));
@@ -1922,8 +1924,8 @@ public class FMRadio extends Activity
          }
       }else if (isCallActive()) {
          if (mRadioTextTV != null) {
-            mRadioTextTV.setText("");
-            mRadioTextScroller.mOriginalString = "";
+            mRadioTextTV.setText(getString(R.string.fm_call));
+            mRadioTextScroller.mOriginalString = getString(R.string.fm_call);
          }
          if (mERadioTextTV != null) {
              mERadioTextTV.setText("");
@@ -1935,7 +1937,7 @@ public class FMRadio extends Activity
          }
       }else {
          if (mRadioTextTV != null) {
-             mRadioTextTV.setText("");
+             mRadioTextTV.setText(getString(R.string.fm_off));
              mRadioTextScroller.mOriginalString = "";
          }
          if (mERadioTextTV != null) {
@@ -2070,7 +2072,6 @@ public class FMRadio extends Activity
       mRadioTextTV.setText("");
       mERadioTextTV.setText("");
       mERadioTextTV.setVisibility(View.GONE);
-      mRadioTextTV.setVisibility(View.GONE);
       mRadioTextScroller.mOriginalString = "";
       mRadioTextScroller.mStringlength = 0;
       mRadioTextScroller.mIteration = 0;
@@ -2565,7 +2566,7 @@ public class FMRadio extends Activity
       mTunedStation.setPI(0);
       mTunedStation.setRDSSupported(false);
       mTunedStation.setPty(0);
-      mRadioTextTV.setText("");
+      mRadioTextTV.setText(getString(R.string.fm_off));
       mERadioTextTV.setText("");
       mRadioTextScroller.mOriginalString = "";
       mProgramServiceTV.setText("");
@@ -2671,7 +2672,7 @@ public class FMRadio extends Activity
                   mRadioTextScroller.mOriginalString = str;
                }else if(TextUtils.isEmpty(str)) { /* Rest the string to empty*/
                   mRadioTextTV.setText("");
-                  mRadioTextScroller.mOriginalString = "";
+                  mRadioTextScroller.mOriginalString = getString(R.string.fm_off);
                }else {
                   Log.v(LOGTAG, "mUpdateRadioText: Leaving old string " + mRadioTextTV.getText());
                }
