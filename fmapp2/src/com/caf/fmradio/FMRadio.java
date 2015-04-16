cb421681fb0f0d1894cebb8af61010f1d0aef9e3
Danesh M <daneshm90@gmail.com>
2015-04-16 10:18:08 -0700

    FM: Launch station list automatically after scan
    Give status update post scaning for categories
    Fix %s showing in stationList
    
    Change-Id: I1845f0cdeb0051bcfb951e2fef791a016fa81ef4

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -234,6 +234,7 @@ public class FMRadio extends Activity
 
    private static boolean mFMStats = false;
 
+   private boolean mShowStationList = false;
 
    /* Current Status Indicators */
    private static boolean mRecording = false;
@@ -1688,6 +1689,9 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
       SharedPreferences.Editor editor = sp.edit();
       editor.clear();
       editor.commit();
+      if (mScannedFrequencies != null) {
+         mScannedFrequencies.clear();
+      }
    }
    public boolean fmConfigure() {
       boolean bStatus = true;
@@ -2668,6 +2672,11 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
          resetFMStationInfoUI();
          invalidateOptionsMenu();
          saveStations();
+         if (mShowStationList) {
+            Intent stationListIntent = new Intent(FMRadio.this, StationListActivity.class);
+            startActivity(stationListIntent);
+         }
+         mShowStationList = false;
       }
    };
 
@@ -3109,6 +3118,20 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
       }
       public void onSearchComplete() {
          Log.d(LOGTAG, "mServiceCallbacks.onSearchComplete :");
+         if (mIsScaning) {
+             if (mScannedFrequencies != null && mScannedFrequencies.size() > 0) {
+                 mShowStationList = true;
+             } else {
+                 mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast t = Toast.makeText(FMRadio.this,
+                           getString(R.string.fm_search_no_results), Toast.LENGTH_SHORT);
+                        t.show();
+                    }
+                 });
+             }
+         }
          mScanPty = 0;
          mScanPtyIndex = 0;
          mIsScaning = false;
