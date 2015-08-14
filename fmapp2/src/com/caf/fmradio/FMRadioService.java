5b8649a8a7b294ac9c0d03af28d9336e1c744917
Danesh M <daneshm90@gmail.com>
2015-08-14 11:20:31 -0700

    FM: Ensure scan state is kept in sync
    In the middle of a scan, the user could navigate away from the activity,
    during which the scan could've finished. The activity is no longer synchronized
    with the service and causes issues where the dialog for scan keeps showing...etc
    
    Fix this by syncing state in onResume
    
    issue-id: CRACKLING-529
    Change-Id: Ief3138e4d6e157ad0b060dc3aced18668959f4e1

--- fmapp2/src/com/caf/fmradio/FMRadioService.java
+++ fmapp2/src/com/caf/fmradio/FMRadioService.java
@@ -2111,6 +2111,11 @@ public class FMRadioService extends Service
          return(mService.get().isA2DPConnected());
       }
 
+      public boolean isSearchInProgress()
+      {
+         return(mService.get().isSearchInProgress());
+      }
+
       public int getExtenCountryCode()
       {
          return(mService.get().getExtenCountryCode());
@@ -2444,6 +2449,11 @@ public class FMRadioService extends Service
       }
       stop();
       return(bStatus);
+   }
+
+   public boolean isSearchInProgress() {
+      int state = mReceiver.getFMState();
+      return state == qcom.fmradio.FmTransceiver.FMState_Srch_InProg;
    }
 
    public boolean isSSRInProgress() {
