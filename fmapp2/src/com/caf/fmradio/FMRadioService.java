bd58b3b8861616e6d1492fcc59185a7bfe1b6445
Danesh M <daneshm90@gmail.com>
2015-08-19 15:43:48 -0700

    FM: Keep track of scanned frequencies in service
    By keeping track of scanned frequencies in the service, this allows
    us to fetch it when the activity is resumed and not rely on having
    active callbacks.
    
    Change-Id: Idea9cab3e5fd2a789333c3f126416e0102613aa8

--- fmapp2/src/com/caf/fmradio/FMRadioService.java
+++ fmapp2/src/com/caf/fmradio/FMRadioService.java
@@ -164,6 +164,7 @@ public class FMRadioService extends Service
    private FMDeathRecipient mFMdr;
 
    //PhoneStateListener instances corresponding to each
+   private ArrayList<Integer> mScannedFrequencies = new ArrayList<Integer>();
 
    private FmRxRdsData mFMRxRDSData=null;
    // interval after which we stop the service when idle
@@ -2116,6 +2117,11 @@ public class FMRadioService extends Service
          return(mService.get().isSearchInProgress());
       }
 
+      public List<Integer> getScannedFrequencies()
+      {
+         return(mService.get().getScannedFrequencies());
+      }
+
       public int getExtenCountryCode()
       {
          return(mService.get().getExtenCountryCode());
@@ -2451,6 +2457,10 @@ public class FMRadioService extends Service
       return(bStatus);
    }
 
+   public List<Integer> getScannedFrequencies() {
+       return mScannedFrequencies;
+   }
+
    public boolean isSearchInProgress() {
       int state = mReceiver.getFMState();
       return state == qcom.fmradio.FmTransceiver.FMState_Srch_InProg;
@@ -2687,6 +2697,8 @@ public class FMRadioService extends Service
     */
    public boolean scan(int pty)
    {
+      // Clear previously scanned frequencies
+      mScannedFrequencies.clear();
       boolean bCommandSent=false;
       if (mReceiver != null)
       {
@@ -3200,6 +3212,9 @@ public class FMRadioService extends Service
             /* Since the Tuned Status changed, clear out the RDSData cached */
             if(mReceiver != null) {
                clearStationInfo();
+            }
+            if (isSearchInProgress()) {
+                mScannedFrequencies.add(frequency);
             }
             if(mCallbacks != null)
             {
