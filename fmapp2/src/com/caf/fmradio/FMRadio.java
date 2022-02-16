LineageOS/android_hardware_qcom_fm

FM: adaptive icon
Change-Id: I95c0cd920c72d7a7dba952dece5f6228dbbf3988
Signed-off-by: Joey <joey@lineageos.org>

fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -1274,7 +1274,7 @@ public class FMRadio extends Activity
           if (mProgressDialog != null) {
               mProgressDialog.setTitle(titleStr);
               mProgressDialog.setMessage(msgStr);
-              mProgressDialog.setIcon(R.mipmap.ic_launcher_fmradio);
+              mProgressDialog.setIcon(R.mipmap.ic_launcher);
               mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
               mProgressDialog.setCanceledOnTouchOutside(false);
               mProgressDialog.setButton(DialogInterface.BUTTON_POSITIVE,

LineageOS/android_vendor_qcom_opensource_fm-commonsys

fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -1221,7 +1221,7 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions,  i
       if (bSearchActive) {
           AlertDialog.Builder builder = new AlertDialog.Builder(this);
           builder.setTitle(titleStr);
-          builder.setIcon(R.drawable.ic_launcher_fmradio);
+          builder.setIcon(R.mipmap.ic_launcher);
           View view = getLayoutInflater().inflate(R.layout.layout_dialog_progress,null);
           builder.setView(view);
           TextView tvMessage = (TextView)view.findViewById(R.id.id_tv_message);